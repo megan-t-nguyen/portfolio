efe6068be22b92a419b944c911f32cfb7f37b73e
Megan Nguyen <megantnguyen@gmail.com>
2026-03-26 23:37:55 -0500

    add thrift-n-stitch app link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,15 @@ export default function Home() {
       <p>⊹₊⟡⋆˚₊‧꒰ა ❤︎ ໒꒱ ‧₊˚⋆⟡₊⊹</p>
       <p>
         <a href="https://megan-nguyen-resume.netlify.app/" target="_blank" rel="noopener noreferrer">
-          ⋆｡°✩ view my resume ✩°｡⋆
+          ☆ view my resume ✩°｡⋆
         </a>
+      </p>
+      <p>
+        <a href="https://thrift-n-stitch.vercel.app/" target="_blank" rel="noopener noreferrer">
+          ♡ thrift-n-stitch -ˋˏ✄┈┈┈┈ 
+        </a>
+        <br />
+        ^^^ thrift flipping app ^^^ 
       </p>
     </>
   );
